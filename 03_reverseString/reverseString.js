26a876a5c5a33f4e95a5e574a50b95b2c9ea3f90
devTylr <tyler.ward1199@gmail.com>
2023-08-19 10:08:23 -0400

    Completed reverseString test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split('');
+    const reverseArr = splitString.reverse();
+    const joinArr = reverseArr.join('');
+    return joinArr;
 };
 
 // Do not edit below this line
