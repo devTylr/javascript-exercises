1fb9f306b1dea15fe13ea7867260c5e93fe9e83a
devTylr <tyler.ward1199@gmail.com>
2023-08-19 09:47:59 -0400

    Completed repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = '';
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            repeated += string;
+        }
+    } else {
+        return 'ERROR';
+    }
+    return repeated;
 };
 
 // Do not edit below this line
