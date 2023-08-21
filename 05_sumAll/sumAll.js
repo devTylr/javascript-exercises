0e2503ffd636d7a732ff845135631a59e4fa6793
devTylr <tyler.ward1199@gmail.com>
2023-08-21 12:20:04 -0400

    found solution for sumAll!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(lowerNum, higherNum) {
+    let finalSum = 0;
 
+    if (lowerNum < 0 
+        || higherNum < 0 
+        || lowerNum === null 
+        || higherNum === null
+        || !Number.isInteger(lowerNum)
+        || !Number.isInteger(higherNum)) {
+        return 'ERROR';
+    }
+    // in case user passes higher number as first argument
+    if (lowerNum > higherNum) {
+        for (let i = higherNum; i <= lowerNum; i++) {
+            finalSum += i;
+        }
+    } else {
+        for (let i = lowerNum; i <= higherNum; i++) {
+            finalSum += i;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
